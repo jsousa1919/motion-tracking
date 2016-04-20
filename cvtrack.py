edb66455f2d40f53496117aa2eb6cbe063a02a62
Justin <jsousa1919@gmail.com>
2016-04-19 22:10:28 -0400

    better speed and background recognition

--- cvtrack.py
+++ cvtrack.py
@@ -9,7 +9,8 @@ import time
 parser = argparse.ArgumentParser()
 parser.add_argument('-f', '--file', help='Path to video, defaults to camera')
 parser.add_argument('-b', '--background-buffer-length', type=int, default=100, help='Number of frames to use as a background averaging buffer')
-parser.add_argument('-w', '--width', default=0, help='Width to resize video frames to, defaults to no resize')
+parser.add_argument('-w', '--width', type=int, default=0, help='Width to resize video frames to, defaults to no resize')
+parser.add_argument('-t', '--threshold', type=int, default=5, help='Threshhold pixel diff for changes')
 args = vars(parser.parse_args())
 
 if args.get('file'):
@@ -18,24 +19,30 @@ else:
     cam = cv2.VideoCapture(0)
     time.sleep(0.5)
 
-backgroundBuffer = []
+backgroundBuffer = None
 one_behind = None
 two_behind = None
 cv2.namedWindow('background')
 cv2.namedWindow('angie')
+cv2.namedWindow('diff')
 background = None
 
-def accumulate_background(bg, two, one):
+def accumulate_background(backgroundBuffer, bg, two, one):
     ret = bg
     diff = cv2.absdiff(two, one)
-    diff = cv2.threshold(diff, 25, 255, cv2.THRESH_BINARY)[1]
+    diff = cv2.threshold(diff, args.get('threshold'), 255, cv2.THRESH_BINARY)[1]
     diff = cv2.dilate(diff, None, iterations=2)
-    backgroundBuffer.append(diff)
+    cv2.imshow('diff', diff)
+    if backgroundBuffer == None:
+        backgroundBuffer = [diff]
+    else:
+        backgroundBuffer = np.append(backgroundBuffer, [diff], 0)
     print len(backgroundBuffer)
     print int(args.get('background_buffer_length'))
-    if len(backgroundBuffer) == int(args.get('background_buffer_length')):
-        backgroundBuffer.pop(0)
+    if len(backgroundBuffer) == args.get('background_buffer_length'):
+        backgroundBuffer = np.delete(backgroundBuffer, 1, axis=0)
         diff = np.sum(backgroundBuffer, axis=0)
+        print np.any(diff)
         ret = bg.copy()
         tmp = bg.copy()
         tmp[diff == 0] = 0
@@ -45,7 +52,7 @@ def accumulate_background(bg, two, one):
         ret = cv2.add(ret, tmp)
         if np.any(bg):
             ret = ret / 2
-    return ret
+    return (backgroundBuffer, ret)
 
 while True:
     (grabbed, frame) = cam.read()
@@ -54,7 +61,7 @@ while True:
         break
 
     if args.get('width'):
-        frame = imutils.resize(frame, width=int(args['width']))
+        frame = imutils.resize(frame, width=args['width'])
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (21, 21), 0)
@@ -67,14 +74,12 @@ while True:
         two_behind = one_behind
         one_behind = gray
     if one_behind != None and two_behind != None:
-        background = accumulate_background(background, two_behind, one_behind)
+        (backgroundBuffer, background) = accumulate_background(backgroundBuffer, background, two_behind, one_behind)
     if np.any(background):
         diff = cv2.absdiff(background, gray)
         diff = cv2.threshold(diff, 25, 255, cv2.THRESH_BINARY)[1]
         diff = cv2.dilate(diff, None, iterations=2)
         cv2.imshow('background', background)
-        if cv2.waitKey(50) % 256 == ord('q'):
-            break
         cv2.imshow('angie', frame)
         if cv2.waitKey(50) % 256 == ord('q'):
             break
